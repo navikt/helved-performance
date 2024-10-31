3c2ba3362d7abc755051ecbfb48aee0f1613be1f
Robin <robin.tordly@nav.no>
2024-10-31 13:43:31 +0100

    må binde til 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> std::io::Result<()> {
             .route("/health", get().to(is_alive))
             .route("/iverksett", get().to(iverksett))
     })
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
