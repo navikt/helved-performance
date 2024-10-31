3b9b355d3215ce57a385abf47a3e48d7c704fd70
Robin <robin.tordly@nav.no>
2024-10-31 15:00:59 +0100

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use actix_web::{App, HttpResponse, HttpServer};
 use actix_web::web::get;
+use actix_web::{App, HttpResponse, HttpServer};
 use dto::*;
+use log::{info, warn, LevelFilter};
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Logger, Root};
+use log4rs::encode::json::JsonEncoder;
+use log4rs::{init_config, Config};
 
-mod dto;
 mod client;
+mod dto;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    init_logger();
     let host = env_or_default("BIND_ADDRESS", "127.0.0.1:8080");
 
     HttpServer::new(|| {
@@ -14,9 +20,8 @@ async fn main() -> std::io::Result<()> {
             .route("/health", get().to(is_alive))
             .route("/iverksett", get().to(iverksett))
     })
-        .bind(host)?
-        .run()
-        .await
+    .bind(&host)?
+    .run().await
 }
 
 pub fn env_or_default(key: &str, default: &str) -> String {
@@ -37,10 +42,29 @@ async fn iverksett() -> HttpResponse {
     vedtak.add_utbetaling(utbetaling);
     iverksetting.set_vedtak(vedtak);
 
+    info!("iverksetter: {:?} ..", iverksetting);
+
     let url = "http://utsjekk-oppdrag/api/iverksetting/v2";
     match client::post(url, &iverksetting).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+        Ok(res) => {
+            info!("response: {:?}", res);
+            HttpResponse::Ok().finish()
+        }
+        Err(err) => {
+            warn!("response: {:?}", err);
+            HttpResponse::InternalServerError().body(err.to_string())
+        }
     }
 }
 
+fn init_logger() {
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(JsonEncoder::new()))
+        .build();
+    let config = Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .logger(Logger::builder().build("app::helved-performance", LevelFilter::Info))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+        .expect("Failed to build log config");
+    init_config(config).expect("Failed to init logger");
+}
